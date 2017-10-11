a0fe904c1755160636311dae11eb2774d61898f4
yoskeoka <yoske95@gmail.com>
2017-10-12 00:43:46 +0900

    fix import sample

--- src/App.tsx
+++ src/App.tsx
@@ -5,58 +5,58 @@ import DeleteIcon from 'material-ui-icons/Delete';
 import { withStyles, WithStyles } from 'material-ui/styles';
 
 const styles = {
-  box: {
-    margin: 10,
-    padding: 10,
-    border: 'solid 1px gray',
-  },
-  button: {
-    margin: 10,
-  },
-  buttonWithHover: {
-    margin: 10,
-    // hoverも記述できる
-    '&:hover': {
-      backgroundColor: '#ff0000',
-    }
-  },
+    box: {
+        margin: 10,
+        padding: 10,
+        border: 'solid 1px gray',
+    },
+    button: {
+        margin: 10,
+    },
+    buttonWithHover: {
+        margin: 10,
+        // hoverも記述できる
+        '&:hover': {
+            backgroundColor: '#ff0000',
+        }
+    },
 };
 
 type ClassNames = keyof typeof styles;
 
 interface FlatButtonsProps {
-  onClick: () => void;
+    onClick: () => void;
 }
 
 const FlatButtonsSample = withStyles(styles)<FlatButtonsProps>(
-  (props: FlatButtonsProps & WithStyles<ClassNames>) => {
-    const classes = props.classes;
-    return (
-      <div className={classes.box}>
-        {/* クリックイベントの処理はこんな感じ */}
-        <Button onClick={props.onClick} className={classes.button}>Default</Button>
-        <Button color="primary" className={classes.buttonWithHover}>Primary</Button>
-        <Button color="accent"><DeleteIcon />削除</Button>
-      </div>
-    );
-  }
+    (props: FlatButtonsProps & WithStyles<ClassNames>) => {
+        const classes = props.classes;
+        return (
+            <div className={classes.box}>
+                {/* クリックイベントの処理はこんな感じ */}
+                <Button onClick={props.onClick} className={classes.button}>Default</Button>
+                <Button color="primary" className={classes.buttonWithHover}>Primary</Button>
+                <Button color="accent"><DeleteIcon />削除</Button>
+            </div>
+        );
+    }
 );
 
 interface RaisedButtonsProps {
-  onClick: () => void;
+    onClick: () => void;
 }
 
 class RaisedButtonsSample extends React.Component<RaisedButtonsProps & WithStyles<ClassNames>, {}> {
-  render() {
-    const classes = this.props.classes;
-    return (
-      <div className={classes.box}>
-        <Button raised={true} onClick={this.props.onClick} className={classes.buttonWithHover}>Default</Button>
-        <Button raised={true} color="primary" className={classes.button}>Primary</Button>
-        <Button raised={true} color="accent" className={classes.button}><DeleteIcon />削除</Button>
-      </div>
-    );
-  }
+    render() {
+        const classes = this.props.classes;
+        return (
+            <div className={classes.box}>
+                <Button raised={true} onClick={this.props.onClick} className={classes.buttonWithHover}>Default</Button>
+                <Button raised={true} color="primary" className={classes.button}>Primary</Button>
+                <Button raised={true} color="accent" className={classes.button}><DeleteIcon />削除</Button>
+            </div>
+        );
+    }
 }
 
 // 1ファイル内で書くためにHOCを変数に入れておく
@@ -69,22 +69,21 @@ const HocRaisedButtonsSample = withStyles(styles)<RaisedButtonsProps>(RaisedButt
 // export default withStyles<{} & ClassNames>(styles)<RaisedButtonsProps>(RaisedButtonsSample);
 
 // そして別ファイルでimport
-// import RaisedButtonsSample from "./components/RaisedButtonsSample.tsx";
+// import RaisedButtonsSample from './components/RaisedButtonsSample.';
 
 class App extends React.Component {
-  handleClick = () => {
-    alert('Clicked!');
-  }
+    handleClick = () => {
+        alert('Clicked!');
+    }
 
-  render() {
-    return (
-      <div>
-
-        <FlatButtonsSample onClick={this.handleClick} />
-        <HocRaisedButtonsSample onClick={this.handleClick} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <FlatButtonsSample onClick={this.handleClick} />
+                <HocRaisedButtonsSample onClick={this.handleClick} />
+            </div>
+        );
+    }
 }
 
 export default App;
